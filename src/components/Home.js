53f7ed980431a75356eec7ad0601dc2864466286
Jfang370 <jimmyfang23@gmail.com>
2023-10-29 19:30:34 -0400

    added services info

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,6 +57,25 @@ class Home extends Component {
               <h1 class='main-info--header'>Manage Your Bank Accounts</h1>
               <h4 class='main-info--subheader'>Access your accounts 24/7, whenever and wherever</h4>
             </div>
+            <div class='main-info__content--services'>
+              <ul>
+                <li>
+                  <img src='https://factohr-1a56a.kxcdn.com/wp-content/uploads/2021/03/hpetpoe.png'></img>
+                  <h2>Pay On The Go</h2>
+                  <p>Make payments, view history and get e-bills. Reduce the hassle of paying with our app.</p>
+                </li>
+                <li>
+                  <img src='https://zilbank.com/wp-content/uploads/2023/06/Understanding-Best-Online-Instant-Check-Cashing-ZB.png'></img>
+                  <h2>Deposite Anywhere</h2>
+                  <p>Deposit checks using your mobile device with React Bank Mobile Deposit. Funds are usually available the next business day</p>
+                </li>
+                <li>
+                <img src='https://website-assets-fd.freshworks.com/attachments/ckjv6jx7700lscjfzuyspsnv0-customer-service-team-training.one-half.png'></img>
+                  <h2>Live Customer Service</h2>
+                  <p>Call 1-111-1111 to talk to a real person any time of the day, any day of the week.</p>
+                </li>
+              </ul>
+            </div>
           </div>
 
         </div>
