451c20e0032bd5b7bad3c8da34f5788a347dea76
Jfang370 <jimmyfang23@gmail.com>
2023-10-29 23:01:41 -0400

    Added DashbaordNav

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,10 +5,12 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import DashboardNav from './DashboardNav';
 
 const Credits = (props) => {
   return (
     <div>
+      <DashboardNav/>
       <h1>Credits</h1>
       <br/>
       <Link to="/">Return to Home</Link>
